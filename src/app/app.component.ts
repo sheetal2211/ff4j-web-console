fe91c6884d98d23ffc42dc43c5c8a767cf8dbeb5
Paul <paul58914080@gmail.com>
2018-09-10 21:37:01 +0530

    fixed the issue with scroll by removing margin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'ff4j-root',
   templateUrl: './app.component.html',
-  styles: ['section { margin-top: 70px}']
+  styles: ['section { padding-top: 70px}']
 })
 export class AppComponent {
 }
