4f486a6fb2f0d28c54f10e3ba602f9ade487d2d5
Paul <paul58914080@gmail.com>
2018-09-16 10:05:11 +0530

    removed unused module

--- src/app/shared/components/nav/nav.module.ts
+++ src/app/shared/components/nav/nav.module.ts
@@ -1,12 +1,10 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {NavComponent} from './nav.component';
 import {MatIconModule, MatToolbarModule} from '@angular/material';
 import {RoutesModule} from '../../../routes/routes.module';
 
 @NgModule({
   imports: [
-    CommonModule,
     MatToolbarModule,
     MatIconModule,
     RoutesModule
