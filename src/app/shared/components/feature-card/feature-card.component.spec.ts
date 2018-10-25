551f99a2aa711a04ddda36ae326088414f41f241
Paul <paul58914080@gmail.com>
2018-10-25 08:47:58 +0530

    Feature to show customProperties

--- src/app/shared/components/feature-card/feature-card.component.spec.ts
+++ src/app/shared/components/feature-card/feature-card.component.spec.ts
@@ -3,46 +3,73 @@ import {Component} from '@angular/core';
 import {Feature} from '../../models/Feature';
 import {initContext, TestContext} from '../../../../testing/test.context';
 import {MatButtonModule, MatCardModule, MatIconModule, MatSlideToggleModule} from '@angular/material';
+import {LoggerTestingModule} from 'ngx-logger';
+import {Property} from '../../models/Property';
 
 @Component({
-    template: `<ff4j-feature-card [feature]='feature'></ff4j-feature-card>`
+  template: `
+    <ff4j-feature-card [feature]='feature'></ff4j-feature-card>`
 })
 class TesteeFeatureCardComponent {
-    feature: Feature = {
-        uid: 'Feature_UID',
-        description: `Feature Card Description`,
-        group: 'Group Name',
-        enable: false,
-        permissions: ['ROLE_ADMIN', 'ROLE_USER', 'ROLE_ADMIN', 'ROLE_USER']
-    };
+  feature: Feature = {
+    uid: 'Feature_UID',
+    description: `Feature Card Description`,
+    group: 'Group Name',
+    enable: false,
+    permissions: [ 'ROLE_ADMIN', 'ROLE_USER', 'ROLE_ADMIN', 'ROLE_USER' ]
+  };
 }
 
 describe('FeatureCardComponent', () => {
-    type Context = TestContext<FeatureCardComponent, TesteeFeatureCardComponent>;
-    const moduleMetaData: any = {
-        MatCardModule,
-        MatSlideToggleModule,
-        MatButtonModule,
-        MatIconModule
-    };
-    initContext(FeatureCardComponent, TesteeFeatureCardComponent, moduleMetaData);
+  type Context = TestContext<FeatureCardComponent, TesteeFeatureCardComponent>;
 
-    it('should create', function (this: Context) {
-        this.fixture.detectChanges();
-        expect(this.hostComponent).toBeTruthy();
-        expect(this.testedComponent).toBeTruthy();
-    });
+  const moduleMetaData = {
+    imports: [
+      MatCardModule,
+      MatSlideToggleModule,
+      MatButtonModule,
+      MatIconModule,
+      LoggerTestingModule
+    ]
+  };
 
-    it('should have got the input value for feature', function(this: Context) {
-        this.fixture.detectChanges();
-        expect(this.testedComponent.feature.uid).toBe('Feature_UID');
-        expect(this.testedComponent.feature.description).toBe(`Feature Card Description`);
-        expect(this.testedComponent.feature.group).toBe('Group Name');
-        expect(this.testedComponent.feature.enable).toBeFalsy();
-        expect(this.testedComponent.feature.permissions.length).toEqual(4);
-        // on change should apply
-        this.hostComponent.feature.enable = true;
-        this.detectChanges();
-        expect(this.testedComponent.feature.enable).toBeTruthy();
+  initContext(FeatureCardComponent, TesteeFeatureCardComponent, moduleMetaData);
+
+  it('should create', function (this: Context) {
+    this.detectChanges();
+    expect(this.hostComponent).toBeTruthy();
+    expect(this.testedComponent).toBeTruthy();
+  });
+
+  it('should have got the input value for feature', function (this: Context) {
+    this.detectChanges();
+    expect(this.testedComponent.feature.uid).toBe('Feature_UID');
+    expect(this.testedComponent.feature.description).toBe(`Feature Card Description`);
+    expect(this.testedComponent.feature.group).toBe('Group Name');
+    expect(this.testedComponent.feature.enable).toBeFalsy();
+    expect(this.testedComponent.feature.permissions.length).toEqual(4);
+    // on change should apply
+    this.hostComponent.feature.enable = true;
+    this.detectChanges();
+    expect(this.testedComponent.feature.enable).toBeTruthy();
+  });
+
+  it('should be able to format and show customProperties in key value', function (this: Context) {
+    // The customProperties needs to be transformed to a Map
+    const properties = new Map<string, Property>();
+    properties.set('SampleProperty', {
+      'name': 'SampleProperty',
+      'description': '',
+      'type': 'org.ff4j.property.PropertyString',
+      'value': 'go!',
+      'fixedValues': []
     });
+    this.detectChanges();
+    let resultValues = this.testedComponent.getPropertiesValues(properties);
+    expect(resultValues).toEqual(['SampleProperty = go!']);
+    // if customProperties is undefined or empty it should also result in empty
+    const testee = undefined;
+    resultValues = this.testedComponent.getPropertiesValues(testee);
+    expect(resultValues).toEqual([]);
+  });
 });
--- src/app/shared/components/feature-card/feature-card.component.ts
+++ src/app/shared/components/feature-card/feature-card.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Feature} from '../../models/Feature';
+import {Property} from '../../models/Property';
+import {NGXLogger} from 'ngx-logger';
 
 @Component({
   selector: 'ff4j-feature-card',
@@ -9,9 +11,23 @@ import {Feature} from '../../models/Feature';
 export class FeatureCardComponent implements OnInit {
   @Input() feature: Feature;
 
-  constructor() {
+  constructor(private logger: NGXLogger) {
   }
 
   ngOnInit() {
+    if (!this.feature || null == this.feature) {
+      this.logger.error('feature property cannot be undefined');
+      throw new Error('feature property cannot be undefined');
+    }
+  }
+
+  getPropertiesValues(customProperties: Map<string, Property>): string[] {
+    const result = [];
+    if (customProperties) {
+      customProperties.forEach((value: Property) => {
+        result.push(value.name + ' = ' + value.value);
+      });
+    }
+    return result;
   }
 }
