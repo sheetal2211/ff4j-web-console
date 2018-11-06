0cb0876a32799ac6418e8955dca1fbfe945035a8
paul58914080 <paul58914080@gmail.com>
2018-11-06 12:28:30 +0530

    integration with heroku

--- src/app/shared/services/feature.service.ts
+++ src/app/shared/services/feature.service.ts
@@ -2,16 +2,19 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Feature} from '../models/Feature';
+import {environment} from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeatureService {
+  baseUrl: string;
 
   constructor(private http: HttpClient) {
+    this.baseUrl = environment.apiBaseUrl ? environment.apiBaseUrl : '';
   }
 
   getFeatures(): Observable<Feature[]> {
-    return this.http.get<Feature[]>('/ff4j/store/features');
+    return this.http.get<Feature[]>(this.baseUrl + '/ff4j/store/features');
   }
 }
