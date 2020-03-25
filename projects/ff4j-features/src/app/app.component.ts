767683780d438c1b0b5bc4a11079b0ef0bdd512e
J-DK <dileepkumarjami@gmail.com>
2020-03-25 16:46:47 +0530

    feat(ff4j-features): created the initial boiler plate code for features sub project

---
+++ projects/ff4j-features/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ff4j-features-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ff4j-features';
+}
