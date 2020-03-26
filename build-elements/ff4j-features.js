3e7d6463068c1913c7cd8fd2e3b0d7869542232d
J-DK <dileepkumarjami@gmail.com>
2020-03-26 13:20:59 +0530

    refactor: used es6 instead of es5 & removed styles

--- build-elements/ff4j-features.js
+++ build-elements/ff4j-features.js
@@ -3,15 +3,11 @@ const concat = require('concat');
 
 (async function build() {
   const files = [
-    './dist/ff4j-features/runtime-es5.js',
-    './dist/ff4j-features/polyfills-es5.js',
-    './dist/ff4j-features/main-es5.js'
+    './dist/ff4j-features/runtime-es2015.js',
+    './dist/ff4j-features/polyfills-es2015.js',
+    './dist/ff4j-features/main-es2015.js'
   ];
 
   await fs.ensureDir('elements');
   await concat(files, 'elements/ff4j-features.js');
-  await fs.copyFile(
-    './dist/ff4j-features/styles.css',
-    'elements/ff4j-features.styles.css'
-  );
 }());
