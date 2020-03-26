98e42618b07d86fd85a7afb4db2944a9db23f38f
J-DK <dileepkumarjami@gmail.com>
2020-03-26 17:26:38 +0530

    feat(features): added the features module to the main app and bootstrapped it in the sub app

--- projects/ff4j-features/src/app/features.module.ts
+++ projects/ff4j-features/src/app/features.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeaturesComponent } from './features.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [FeaturesComponent],
   imports: [CommonModule, BrowserModule],
   entryComponents: [FeaturesComponent],
+  exports: [
+    FeaturesComponent
+  ],
+  bootstrap: environment.production ? [] : [FeaturesComponent]
 })
 export class FeaturesModule {}
