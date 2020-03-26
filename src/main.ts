3349934b7ead9caf64c6668935ecc07c80294514
J-DK <dileepkumarjami@gmail.com>
2020-03-26 11:00:37 +0530

    refactor(custom-element): added the custom element to the main app

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// Uncomment this after building the element
+// import 'elements/ff4j-features';
+
 if (environment.production) {
   enableProdMode();
 }
