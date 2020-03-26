58e2dcfe704b98132cd14d281fb7a2108cf585b2
J-DK <dileepkumarjami@gmail.com>
2020-03-26 17:42:40 +0530

    refactor: deleted the elements folder before concatenation

--- build-elements/ff4j-features.js
+++ build-elements/ff4j-features.js
@@ -1,5 +1,6 @@
 const fs = require('fs-extra');
 const concat = require('concat');
+const del = require('del');
 
 (async function build() {
   const files = [
@@ -8,6 +9,7 @@ const concat = require('concat');
     './dist/ff4j-features/main-es2015.js'
   ];
 
+  await del(['elements/ff4j-features.js']);
   await fs.ensureDir('elements');
   await concat(files, 'elements/ff4j-features.js');
 }());
