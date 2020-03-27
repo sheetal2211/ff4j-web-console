6c4e0a3392ca92496c1af0c2e8b137c0b2b7df02
sheetal2211 <sheetal.agarwal2211@gmail.com>
2020-03-27 18:46:19 +0530

    feat(dashboard-elements): building dashboard module as angular elements

--- projects/ff4j-dashboard/src/app/dashboard.module.ts
+++ projects/ff4j-dashboard/src/app/dashboard.module.ts
@@ -1,15 +1,24 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './dashboard.component';
-import { BrowserModule } from '@angular/platform-browser';
+import { Injector, NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { DashboardComponent } from "./dashboard.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { createCustomElement } from "@angular/elements";
+import { environment } from "../environments/environment";
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [CommonModule, BrowserModule],
   entryComponents: [DashboardComponent],
-  bootstrap: [DashboardComponent],
-  exports: [
-    DashboardComponent
-  ]
+  bootstrap: environment.loadBootstrap ? [DashboardComponent] : [],
+  exports: [DashboardComponent],
 })
-export class DashboardModule {}
+export class DashboardModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const dashboardElement = createCustomElement(DashboardComponent, {
+      injector: this.injector,
+    });
+    customElements.define("ff4j-dashboard-element", dashboardElement);
+  }
+}
