6ba786b079c0e626c6b35e31c485e58c15f1a55e
sheetal2211 <sheetal.agarwal2211@gmail.com>
2020-03-27 19:22:47 +0530

    feat(dashboard-elements): dashboard elements build file added

---
+++ build-elements/ff4j-dashboard.js
@@ -0,0 +1,15 @@
+const fs = require("fs-extra");
+const concat = require("concat");
+const del = require("del");
+
+(async function build() {
+  const files = [
+    "./dist/ff4j-dashboard/runtime-es2015.js",
+    "./dist/ff4j-dashboard/polyfills-es2015.js",
+    "./dist/ff4j-dashboard/main-es2015.js",
+  ];
+
+  await del(["elements/ff4j-dashboard.js"]);
+  await fs.ensureDir("elements");
+  await concat(files, "elements/ff4j-dashboard.js");
+})();
